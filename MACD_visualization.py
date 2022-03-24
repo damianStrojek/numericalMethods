1f2d7f1e1713c0480a7b484e82a34502008cacb3
damianFresh <67586060+damianStrojek@users.noreply.github.com>
2022-03-24 21:03:40 +0100

    Update MACD_visualization.py

--- MACD_visualization.py
+++ MACD_visualization.py
@@ -1,14 +1,15 @@
 # Damian Strojek s184407
 import csv
-import os
 from math import floor
 from matplotlib import pyplot
 from dateutil import parser
+
 
 class Macd:
     __macd_series = []
     __signal_series = []
     __buy_sell_signals = []
+    __buy_sell_signals_delayed = []
     __samples = []
 
     def __init__(self, n, samples):
@@ -16,6 +17,7 @@ class Macd:
         self.calculate_macd(n, samples)
         self.calculate_signal()
         self.calculate_buy_sell_signals()
+        self.calculate_buy_sell_signals_delayed()
 
     def calculate_ema(self, period, samples, day):
         alpha = 2 / (period + 1)
@@ -48,45 +50,78 @@ class Macd:
         tmp_macd = self.get_macd(35)
         tmp_signals = self.get_signal()
         for i in range(1, len(tmp_macd)):
-            if tmp_macd[i-1] >= tmp_signals[i-1] and tmp_macd[i] < tmp_signals[i] and tmp_macd[i] < 0:
+            if tmp_macd[i - 1] > tmp_signals[i - 1] and tmp_macd[i] <= tmp_signals[i]:
                 self.__buy_sell_signals.append("sell")
-            elif tmp_macd[i-1] <= tmp_signals[i-1] and tmp_macd[i] > tmp_signals[i] and tmp_macd[i] > 0:
+            elif tmp_macd[i - 1] < tmp_signals[i - 1] and tmp_macd[i] >= tmp_signals[i]:
                 self.__buy_sell_signals.append("buy")
             else:
                 self.__buy_sell_signals.append("nothing")
 
-    def get_macd(self, i = 0):
+    def calculate_buy_sell_signals_delayed(self):
+        self.__buy_sell_signals_delayed = ["nothing"]
+        tmp_macdD = self.get_macd(35)
+        tmp_signalsD = self.get_signal()
+        for i in range(2, len(tmp_macdD)):
+            if tmp_macdD[i - 2] > tmp_signalsD[i] and tmp_macdD[i] <= tmp_signalsD[i]:
+                self.__buy_sell_signals_delayed.append("sell")
+            elif tmp_macdD[i - 2] < tmp_signalsD[i] and tmp_macdD[i] >= tmp_signalsD[i]:
+                self.__buy_sell_signals_delayed.append("buy")
+            else:
+                self.__buy_sell_signals_delayed.append("nothing")
+
+    def get_macd(self, i=0):
         return self.__macd_series[i::]
 
-    def get_signal(self, i = 0):
+    def get_signal(self, i=0):
         return self.__signal_series[i::]
 
-    def get_buy_sell_signals(self, i = 0):
+    def get_buy_sell_signals(self, i=0):
         return self.__buy_sell_signals[i::]
+
+    def get_buy_sell_signals_delayed(self, i=0):
+        return self.__buy_sell_signals_delayed[i::]
 
     def debug(self):
         for i in self.__samples:
             print(i)
 
 
-class Visualization:
-    def diagram_macd(self, time, macd, signal):
-        pyplot.plot(time, macd, label="MACD", color="green")
-        pyplot.plot(time, signal, label="Signal", color="red")
-        pyplot.legend()
-        pyplot.grid(True)
-        pyplot.ylabel('Wartość składowych')
-        pyplot.xlabel('Data (yyyy-mm)')
-        pyplot.title('Wskaźnik MACD')
-        pyplot.show()
+def diagram_macd(time, macd, signal):
+    pyplot.plot(time, macd, label="MACD", color="green")
+    pyplot.plot(time, signal, label="Signal", color="red")
+    pyplot.legend()
+    pyplot.grid(True)
+    pyplot.ylabel('Wartość składowych')
+    pyplot.xlabel('Data (yyyy-mm)')
+    pyplot.title('Wskaźnik MACD')
+    pyplot.show()
 
-    def diagram_samples(self, time, data):
-        pyplot.plot(time, data, label="macd", color='green')
-        pyplot.ylabel('Kurs USD')
-        pyplot.xlabel('Data (yyyy-mm)')
-        pyplot.title('Kurs akcji Coca-Cola')
-        pyplot.grid(True)
-        pyplot.show()
+# not working as I would like it to work
+def diagram_macd_buy_sell(time, macd, signal, buy_sell):
+    pyplot.plot(time, macd, label="MACD", color="green")
+    pyplot.plot(time, signal, label="Signal", color="red")
+    pyplot.legend()
+    pyplot.grid(True)
+    pyplot.ylabel('Wartość składowych')
+    pyplot.xlabel('Data (yyyy-mm)')
+    pyplot.title('Wskaźnik MACD BUY SELL')
+    for i in range(1, len(buy_sell)):
+        x = [time[i], -5]
+        y = [time[i], 1]
+        if buy_sell[i] == "buy":
+            pyplot.plot(x, y, 'b', linewidth=1, color="blue")
+        elif buy_sell[i] == "sell":
+            pyplot.plot(x, y, 'b', linewidth=1, color="blue")
+    pyplot.show()
+
+
+def diagram_samples(time, data):
+    pyplot.plot(time, data, label="macd", color='green')
+    pyplot.ylabel('Kurs USD')
+    pyplot.xlabel('Data (yyyy-mm)')
+    pyplot.title('Kurs akcji Coca-Cola')
+    pyplot.grid(True)
+    pyplot.show()
 
 
 def read_data(filename):
@@ -102,7 +137,7 @@ def read_data(filename):
 
 def simulation(samples, signals, money):
     currency = 0
-    for i in range(len(samples)):
+    for i in range(len(signals)):
         if signals[i] == "buy":
             if money != 0:
                 currency = float(money / samples[i])
@@ -112,6 +147,7 @@ def simulation(samples, signals, money):
                 money = currency * samples[i]
                 currency = 0
     if money == 0:
+        # -1 is the last element
         return float(currency * samples[-1])
     else:
         return money
@@ -126,8 +162,16 @@ if __name__ == '__main__':
     print("Kapitał początkowy : " + str(starting_money))
     money_simulation = simulation(samples[35::], macd.get_buy_sell_signals(), starting_money)
     profit = floor(money_simulation / starting_money * 100) - 100
-    print("Kapitał końcowy : " + str(money_simulation))
+    print("Kapitał końcowy z algorytmu podstawowego : " + str(money_simulation))
     print("Profit : " + str(profit) + "%")
 
-    Visualization.diagram_macd(Visualization(), time[35::], macd.get_macd(35), macd.get_signal())
-    Visualization.diagram_samples(Visualization(), time[35::], samples[35::])
+    print("Kapitał początkowy : " + str(starting_money))
+    money_simulation = simulation(samples[35::], macd.get_buy_sell_signals_delayed(), starting_money)
+    profit = floor(money_simulation / starting_money * 100) - 100
+    print("Kapitał końcowy z opóźnionego algorytmu MACD : " + str(money_simulation))
+    print("Profit : " + str(profit) + "%")
+
+    diagram_macd(time[35::], macd.get_macd(35), macd.get_signal())
+    diagram_samples(time[35::], samples[35::])
+    #diagram_macd_buy_sell(time[35::], macd.get_macd(35), macd.get_signal(), macd.get_buy_sell_signals())
+
